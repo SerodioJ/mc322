79d1d9d06bff97cebda5e3f089d2e9e33bee4f96
Oozaku <48293220+Oozaku@users.noreply.github.com>
2019-05-27 19:43:55 -0300

    Add files via upload
    Atualização da interface IAnimation

--- projeto/src/pt/componentes/userinterface/IAnimation.java
+++ projeto/src/pt/componentes/userinterface/IAnimation.java
@@ -1,5 +1,18 @@
-package pt.componentes.userinterface;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.userinterface;
 
+/**
+ *
+ * @author yukiharu
+ */
 public interface IAnimation {
-
+    public void story(String[] falas, String[] personagem);
+    public void setWindowName(String name);
+    public void setTempo(String v);
+    public void setPacientName(String pacName);
+    public void setDocName(String docName);
 }
