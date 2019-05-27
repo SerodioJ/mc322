3e2293cce9647c71de029694e1bfb4782a646fb9
Oozaku <48293220+Oozaku@users.noreply.github.com>
2019-05-27 19:45:08 -0300

    Update IAnimation.java

--- projeto/src/pt/componentes/userinterface/IAnimation.java
+++ projeto/src/pt/componentes/userinterface/IAnimation.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pt.userinterface;
+package pt.componentes.userinterface;
 
-/**
- *
- * @author yukiharu
- */
 public interface IAnimation {
     public void story(String[] falas, String[] personagem);
     public void setWindowName(String name);
